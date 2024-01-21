b2f498f4f16ae152ced95cc8e211f241e4bece16
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-01-21 11:07:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/info/MovieInfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ImageCard from "../display/ImageCard";
+import MovieDetails from "./MovieDetails";
+
+const MovieInfo = (props) => {
+  let { MovieDetail, genreArr, id } = props;
+  return (
+    <div>
+      <div className="flex flex-row flex-wrap place-content-center items-center mb-10 mt-5">
+        <ImageCard MovieDetail={MovieDetail} />
+        <MovieDetails MovieDetail={MovieDetail} genreArr={genreArr} />
+      </div>
+      <div className="pt-2 pb-8 flex justify-center">
+        <iframe
+          className="w-4/5 aspect-video"
+          src={"https://vidsrc.to/embed/movie/" + id}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen={true}
+        ></iframe>
+        {/* src={"https://autoembed.to/movie/tmdb/" + id} */}
+      </div>
+    </div>
+  );
+};
+
+export default MovieInfo;
